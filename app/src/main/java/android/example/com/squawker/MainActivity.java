280671151692d81f05675f44d56719749af3edb5
Amit Grover <amitg90@gmail.com>
2018-06-02 11:40:06 -0700

    Completed Custome message with unqiue Key

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -86,21 +86,12 @@ public class MainActivity extends AppCompatActivity implements
         // Start the loader
         getSupportLoaderManager().initLoader(LOADER_ID_MESSAGES, null, this);
 
-        Log.e("MainActivity", "Checking 2");
-
         //
-        if (savedInstanceState != null) {
-            Log.e("MainActivity", "Checking 1");
+        Bundle bundle = getIntent().getExtras();
 
-            Bundle bundle = getIntent().getExtras();
-
-            Log.e("MainActivity", bundle.toString());
-
-            if (bundle != null && bundle.containsKey("Amit-test") == true) {
-                Log.e("MainActivity", bundle.getString("Amit-test"));
-            }
+        if (bundle != null && bundle.containsKey("Amit-test") == true) {
+            Log.e("MainActivity", bundle.getString("Amit-test"));
         }
-
     }
 
     @Override
